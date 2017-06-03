d2c988e2c43b7fde489e0cdb0cf603b15e34e44c
kevin <kevinlllr16@gmail.com>
2017-06-03 07:56:24 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,9 @@ const route=express.Router();
 const bodyParser=require('body-parser');
 const methodOverride=require('method-override');
 const apiRoute=require('./routers').api;
-
-
+const path=require('path');
+const frontEndPath=__dirname+"/../frontend";
+console.log(frontEndPath);
 mongoose.connect('localhost:27017/test2');
 
 
@@ -15,7 +16,17 @@ app.use(methodOverride());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use('/', express.static(frontEndPath));
 
 app.use('/api',apiRoute(express));
 
+app.use('/catalogo',(req,res)=>{
+	res.sendFile('index.html',{root:frontEndPath});
+})
+app.use('/perfil',(req,res)=>{
+	res.sendFile('perfil.html',{root:frontEndPath});
+})
+app.use('/admin',(req,res)=>{
+	res.sendFile('admin.html',{root:frontEndPath});
+})
 module.exports=app;
