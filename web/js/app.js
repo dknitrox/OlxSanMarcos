d9cbbff1faa0c266ff72ead65edb8fee310ae5fd
kevin <kevinlllr16@gmail.com>
2017-06-10 13:30:52 -0500

    update

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,18 @@
 var app=angular.module('app',[]);
 
-app.controller('adminController',function($scope){
+app.controller('adminController',function($scope,$http){
 	$scope.name="kevin";
+	$scope.apellido="mendoza";
+	$scope.users={};
+	$http({
+  method: 'GET',
+  url: '/api/user'
+}).then(function successCallback(response) {
+	$scope.users=response.data;
+	console.log(response.data);
+  }, function errorCallback(response) {
+	console.log(response);
+  });
 });
 app.controller('perfilController',function($scope){
 	$scope.perfil="perfil controller";
